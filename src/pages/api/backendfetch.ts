c1b2f830aab56b2a2ade7fd8984f9f797f25e19c
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2024-04-11 19:21:20 +0530

    chore: rm node-cache

--- src/pages/api/backendfetch.ts
+++ src/pages/api/backendfetch.ts
@@ -1,20 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axiosFetch from "@/Utils/fetch";
-import NodeCache from "node-cache";
-
-const cache = new NodeCache({ stdTTL: 3600 });
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>,
 ) {
-  const cacheKey = JSON.stringify(req?.query);
-
-  // Check if the result for this query is already cached
-  const cachedResult = cache.get(cacheKey);
-  if (cachedResult) {
-    return res.status(200).json(cachedResult);
-  }
   const {
     requestID,
     id,
@@ -54,7 +44,6 @@ export default async function handler(
     season,
     episode,
   });
-  cache.set(cacheKey, result);
   console.log({ result });
   res?.status(200)?.json(result);
   // res.status(200).json({ name: "John Doe" });
